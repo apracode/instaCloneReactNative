4b084c170a7ec814f0daaf9211dddd1c442e468b
annapry <annaprilipko13@gmail.com>
2020-07-29 13:27:20 +0300

    select and take photo

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,18 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { Text, ActivityIndicator } from "react-native";
+import { Text, ActivityIndicator, Image } from "react-native";
 import styled from "styled-components/native";
 import * as Permissions from "expo-permissions";
 import * as MediaLibrary from "expo-media-library";
+import constants from "../../constants";
+import {
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
 const SelectPhoto = () => {
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
+  const [selected, setSelected] = useState();
+  const [allPhotos, setAllPhotos] = useState();
+
+  const getPhotos = async () => {
+    try {
+      const { assets } = await MediaLibrary.getAssetsAsync();
+      const [firstPhoto] = assets;
+      setSelected(firstPhoto);
+      setAllPhotos(assets);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       if (status === "granted") {
         setHasPermission(true);
+        getPhotos();
       }
     } catch (e) {
       console.log(e);
@@ -32,7 +52,35 @@ const SelectPhoto = () => {
         <ActivityIndicator />
       ) : (
         <View>
-          <Text>{hasPermission ? "Thanks" : "Oops"}</Text>
+          {hasPermission ? (
+            <>
+              <Image
+                style={{ width: constants.width, height: constants.height / 2 }}
+                resizeMode={"contain"}
+                source={{ uri: selected && selected.uri }}
+              />
+              <FlatList
+                data={allPhotos}
+                numColumns={3}
+                renderItem={({ item }) => {
+                  return (
+                    <TouchableOpacity onPress={() => setSelected(item)}>
+                      <Image
+                        style={{
+                          width: constants.width / 3,
+                          height: constants.height / 6,
+                          opacity: item.id === selected.id ? 0.5 : 1,
+                        }}
+                        source={{ uri: item.uri }}
+                      />
+                    </TouchableOpacity>
+                  );
+                }}
+              />
+            </>
+          ) : (
+            <Text>"Oops"</Text>
+          )}
         </View>
       )}
     </View>
@@ -42,7 +90,5 @@ const SelectPhoto = () => {
 export default SelectPhoto;
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,23 +1,121 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import styled from "styled-components/native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
+import { Camera } from "expo-camera";
+import constants from "../../constants";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const TakePhoto = ({ navigation }) => {
+  const cameraRef = useRef();
+  const [loading, setLoading] = useState(true);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
+
+  const toggleCameraType = () => {
+    if (cameraType === Camera.Constants.Type.back) {
+      setCameraType(Camera.Constants.Type.front);
+    } else {
+      setCameraType(Camera.Constants.Type.back);
+    }
+  };
+
+  const takePhoto = async () => {
+    if (!canTakePhoto) {
+      return;
+    }
+    try {
+      setCanTakePhoto(false);
+      const { uri } = await cameraRef.current.takePictureAsync({
+        quality: 1,
+      });
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      console.log(uri);
+    } catch (error) {
+      console.log(error);
+      setCanTakePhoto(true);
+    }
+  };
+
+  const askPermission = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status === "granted") {
+        setHasPermission(true);
+      }
+    } catch (e) {
+      console.log(e);
+      setHasPermission(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    askPermission();
+  }, []);
+
   return (
-    <View>
-      <Text>TakePhoto</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Upload")}>
-        <Text>Upload</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      {loading ? (
+        <ActivityIndicator />
+      ) : hasPermission ? (
+        <>
+          <Camera
+            ref={cameraRef}
+            type={cameraType}
+            style={{
+              width: constants.width,
+              height: constants.height / 2,
+              justifyContent: "flex-end",
+              alignItems: "flex-end",
+            }}
+          >
+            <TouchableOpacity onPress={toggleCameraType}>
+              <MaterialIcons
+                style={{ marginBottom: 10, marginRight: 10 }}
+                name="switch-camera"
+                size={30}
+                color="white"
+              />
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 100,
+                alignSelf: "center",
+              }}
+              onPress={takePhoto}
+              disabled={!canTakePhoto}
+            >
+              <View
+                style={{
+                  alignSelf: "center",
+                  width: 100,
+                  height: 100,
+                  borderRadius: 50,
+                  backgroundColor: "#efeeef",
+                  borderWidth: 15,
+                  borderColor: "grey",
+                  borderStyle: "solid",
+                }}
+              ></View>
+            </TouchableOpacity>
+          </View>
+        </>
+      ) : null}
+    </>
   );
 };
 
 export default TakePhoto;
 
 const View = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
-  flex: 1;
 `;
